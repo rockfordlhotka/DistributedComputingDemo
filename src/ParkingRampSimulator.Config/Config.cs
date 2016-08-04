6c8b3f77495d1d230eb81d4702c5fa1f8eb236f5
Rockford Lhotka <rocky@lhotka.net>
2016-08-04 18:19:50 -0500

    Write status messages to Azure table storage

---
+++ src/ParkingRampSimulator.Config/Config.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingRampSimulator
+{
+    public class Config
+    {
+        static string queueConnectionString;
+        static string storageConnectionString;
+
+        public static string GetQueueConnectionString()
+        {
+            if (!string.IsNullOrWhiteSpace(queueConnectionString))
+                return queueConnectionString;
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (var file = File.OpenText(path + @"\parkingsim.config"))
+            {
+                queueConnectionString = file.ReadLine();
+                return queueConnectionString;
+            }
+        }
+
+        public static string GetStorageConnectionString()
+        {
+            if (!string.IsNullOrWhiteSpace(storageConnectionString))
+                return storageConnectionString;
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (var file = File.OpenText(path + @"\parkingsim.config"))
+            {
+                file.ReadLine();
+                storageConnectionString = file.ReadLine();
+                return storageConnectionString;
+            }
+        }
+    }
+}
