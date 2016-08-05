8434dfb8852aec9ee56f914f5513cc4899a87873
Rockford Lhotka <rocky@lhotka.net>
2016-08-04 21:47:14 -0500

    Add web app to expose Facility service

---
+++ src/ParkingRampSimulator.Entities/ConstructorStatusEntity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace ParkingRampSimulator.Entities
+{
+    public class ConstructStatusEntity : TableEntity
+    {
+        public ConstructStatusEntity()
+        { }
+
+        public ConstructStatusEntity(string name)
+        {
+            PartitionKey = "1";
+            RowKey = name;
+        }
+
+        public DateTime TimeStamp { get; set; }
+        public int TotalLocations { get; set; }
+        public int OpenLocations { get; set; }
+        public int InQueueLength { get; set; }
+        public int OutQueueLength { get; set; }
+    }
+}
---
+++ src/ParkingRampSimulatorServices/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ParkingRampSimulatorServices.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
