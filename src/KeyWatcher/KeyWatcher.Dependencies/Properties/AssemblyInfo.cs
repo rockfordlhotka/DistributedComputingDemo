d7b1e6d4923b3f4825df0ad66029f8119296c7cb
Jason Bock <jason@jasonbock.net>
2017-11-05 11:46:28 -0600

    Reverted back to a working version.

---
+++ src/KeyWatcher/KeyWatcher.Dependencies/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("KeyWatcher.Dependencies")]
+[assembly: AssemblyProduct("KeyWatcher.Dependencies")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("KeyWatcher.Orleans.Host")]
