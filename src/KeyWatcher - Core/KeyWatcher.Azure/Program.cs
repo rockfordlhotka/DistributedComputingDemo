d485a378ef81b93f59bed246b8713088db4c476c
Jason Bock <jason@jasonbock.net>
2017-11-10 23:39:13 -0600

    Well, it builds :). Gotta sleep, will check later!

--- src/KeyWatcher - Core/KeyWatcher.Azure/Program.cs
+++ src/KeyWatcher - Core/KeyWatcher.Azure/Program.cs
@@ -1,18 +1,42 @@
 ﻿using Autofac.Extensions.DependencyInjection;
+using KeyWatcher.Orleans.Grains;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Orleans.Hosting;
+using Orleans.Runtime.Configuration;
+using System;
+using System.Threading.Tasks;
 
 namespace KeyWatcher.Azure
 {
 	public static class Program
 	{
-		public static void Main(string[] args) =>
-			Program.BuildWebHost(args).Run();
+		public static async Task Main(string[] args)
+		{
+			var webHost = Program.BuildWebHost(args);
+			var siloHost = Program.BuildSiloHost(webHost.Services);
+			await siloHost.StartAsync();
+			await webHost.RunAsync();
+		}
 
 		public static IWebHost BuildWebHost(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
 				.ConfigureServices(services => services.AddAutofac())
 				.UseStartup<Startup>()
 				.Build();
+
+		private static ISiloHost BuildSiloHost(IServiceProvider serviceProvider)
+		{
+			var configuration = ClusterConfiguration.LocalhostPrimarySilo();
+			configuration.AddMemoryStorageProvider("Default");
+			configuration.AddMemoryStorageProvider("PubSubStore");
+			configuration.AddSimpleMessageStreamProvider("NotificationStream");
+
+			return new SiloHostBuilder()
+				.UseConfiguration(configuration)
+				.UseServiceProviderFactory(services => serviceProvider)
+				.AddApplicationPartsFromReferences(typeof(UserGrain).Assembly)
+				.Build();
+		}
 	}
 }
