9faf3e608304bbf8e7015ef23550bdf84878e159
Rockford Lhotka <rocky@lhotka.net>
2018-03-15 23:49:42 -0700

    Add NetCoreClient project.

---
+++ src/ParkingSim/NetCoreClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NetCoreClient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var status = GetStatus().Result;
+      Console.WriteLine(status);
+    }
+
+    public static async Task<string> GetStatus()
+    {
+      var client = new HttpClient(new HttpClientHandler
+        { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
+      var json = await client.GetStringAsync("http://parkingsim.azurewebsites.net/api/FacilityStatus");
+      var obj = JsonConvert.DeserializeObject<FacilityStatus>(json);
+      return string.Format("Facility is {0:0.#} available", obj.PercentageOpen);
+    }
+  }
+}
