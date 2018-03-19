e77d41075080f8e4b0898522076d4b44069d9f23
Rockford Lhotka <rocky@lhotka.net>
2018-03-18 21:45:40 -0500

    Add XF app for MacOS and a console app for .NET Core.

---
+++ src/ParkingSim/ParkingRampApp/NetCoreClient/FacilityStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreClient
+{
+    public class FacilityStatus
+    {
+        public int TotalLocations { get; set; }
+        public int OpenLocations { get; set; }
+        public double PercentageOpen { get; set; }
+    }
+}
