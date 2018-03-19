b85ab432fe65430ca87f7b6e2c8f1d48fb7b6c48
Rockford Lhotka <rocky@lhotka.net>
2018-03-18 21:17:42 -0500

    Update to XF 2.5 and add wasm project using Ooui.

---
+++ src/ParkingSim/ParkingRampApp/ParkingRampApp.Wasm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Ooui;
+using Xamarin.Forms;
+
+namespace ParkingRampApp.Wasm
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Initialize Xamarin.Forms
+      Forms.Init();
+
+      // Create the UI
+      var page = new ParkingRampApp.MainPage();
+
+      // Publish a root element to be displayed
+      UI.Publish("/", page.GetOouiElement());
+    }
+  }
+}
